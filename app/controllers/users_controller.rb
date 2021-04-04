3a8ce6a26556ae217f5771fb9efbd33f99bf6dc1
kazuhiro_takahashi <hamu.santa+kazu@gmail.com>
2021-04-04 22:55:13 +0000

    5.5.7-user_create-finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,28 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-  end 
-  def new
   end
   
+  def new
+    @user = User.new 
+    #ユーザーオブジェクトを作成し、インスタンス変数に代入。
+    #「空のuserオブジェクト」を作成して、「その空のオブジェクト」にuser新規作成画面で入力した項目を組み込んでくれる
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = '新規作成に成功しました。'
+      redirect_to @user
+    else
+      render :new
+    end  
+  end 
+  
+  
+  private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end 
 end
