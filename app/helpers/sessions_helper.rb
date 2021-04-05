ac9b888a8f4465f2d4ccc4a9dd641162d904c7ec
kazuhiro_takahashi <hamu.santa+kazu@gmail.com>
2021-04-05 15:01:17 +0000

    6.4-finish

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,20 @@
 module SessionsHelper
   
+  # 引数に渡されたユーザーオブジェクトでログインします。
+  # cokiesにuser.idを一時的に保存することができる。ブラウザを閉じたら、消える。
   def log_in(user)
     session[:user_id] = user.id
-  end 
+  end
+
+  # 現在ログイン中のユーザーがいる場合オブジェクトを返します。
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
   
+  # 現在ログイン中のユーザーがいればtrue、そうでなければfalseを返します。
+  def logged_in?
+    !current_user.nil?
+  end
 end
