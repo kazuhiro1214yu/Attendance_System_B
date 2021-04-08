bdfedb119e04cf455264f1c8faf3bec945835ca0
kazuhiro_takahashi <hamu.santa+kazu@gmail.com>
2021-04-08 10:33:15 +0000

    7.3-login-finish

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password]) #userがtrueで、かつそのuserのパスワードが正しい場合、
       log_in user
-      remember user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user) #ログイン画面で「ログイン情報保持のチェックボックス」をチェックしたか否か
       redirect_to user
     else
       flash.now[:danger] = "認証に失敗しました。"
