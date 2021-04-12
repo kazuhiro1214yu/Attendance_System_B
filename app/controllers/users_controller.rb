cf299e01df4970b8d8e20f0d8ee0e762ad9ec121
kazuhiro_takahashi <hamu.santa+kazu@gmail.com>
2021-04-12 07:31:31 +0000

    8.2.2-finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
   before_action :logged_in_user, only: [:show, :edit, :update]
+  before_action :correct_usre, only: [:edit, :update]
 
   def show
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])    before_actionのset_userで「@user」をまとめて定義したので不要
   end
   
   def new
@@ -23,11 +25,11 @@ class UsersController < ApplicationController
   end 
   
   def edit
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])    before_actionのset_userで「@user」をまとめて定義したので不要
   end 
   
   def update
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])    before_actionのset_userで「@user」をまとめて定義したので不要
     if @user.update_attributes(user_params)
       flash[:success] = "ユーザー情報を更新しました。"
       redirect_to @user
@@ -43,11 +45,22 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end 
     
+    # paramsハッシュからユーザーを取得します。    
+    def set_user
+      @user = User.find(params[:id])
+    end 
     
+    # ログイン済みのユーザーか確認します。
     def logged_in_user
       unless logged_in?
         flash[:danger] = "ログインしてください。"
         redirect_to login_url
       end
     end
+    
+    # アクセスしたユーザーが現在ログインしているユーザーか確認します。
+    def correct_user
+      # @user = User.find(params[:id])    before_actionのset_userで「@user」をまとめて定義したので不要
+      redirect_to(root_url) unless current_user?(@user)
+    end 
 end
