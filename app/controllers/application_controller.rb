2903c5327455e5cd64bcf25dcfd0e939b6852582
kazuhiro_takahashi <hamu.santa+kazu@gmail.com>
2021-04-19 03:29:04 +0000

    10.4--finish

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,18 +10,23 @@ class ApplicationController < ActionController::Base
   # ページ出力前に1ヶ月分のデータの存在を確認・セットします。
   def set_one_month 
     # 月の初日を取得。「Date.current」は「当日」を取得できます。そこにrailsメソッドの「beginning_of_month」を繋ぐと「当月の初日」を取得できる。
-    @first_day = Date.current.beginning_of_month
+      # showアクションの「前月へ、翌月へボタン」を押した場合、「前月、翌月のデータ」をparamsに含めて情報を送る
+      # そのため、まずは「そのparamsデータがあるかどうか？」を確認し、なければ「Date.current.beginning_of_month」で当月データを取得。
+      # なければ「params[:date].to_date」で、paramsで送られてきた「前月、翌月」の情報を元に、@first_dayに入れる。
+    @first_day = params[:date].nil? ?
+    Date.current.beginning_of_month : params[:date].to_date
     # @first_day（つまり「当月の初日」）を取得することで、railsメソッドの「end_of_month」で「当月の終日」を取得できる。
     @last_day = @first_day.end_of_month
     one_month = [*@first_day..@last_day] # 対象の月の日数を代入します。
     # ユーザーに紐付く一ヶ月分のレコードを検索し取得します。
-    @attendances = @user.attendances.where(worked_on: @first_day..@last_day)
+    @attendances = @user.attendances.where(worked_on: @first_day..@last_day).order(:worked_on)
 
     unless one_month.count == @attendances.count # それぞれの件数（日数）が一致するか評価します。
       ActiveRecord::Base.transaction do # トランザクションを開始します。
         # 繰り返し処理により、1ヶ月分の勤怠データを生成します。
         one_month.each { |day| @user.attendances.create!(worked_on: day) }
       end
+      @attendances = @user.attendances.where(worked_on: @first_day..@last_day).order(:worked_on)
     end
 
   rescue ActiveRecord::RecordInvalid # トランザクションによるエラーの分岐です。
